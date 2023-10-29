071f2674fd8fb1eb8514ba9915a4d8807f251923
Karolina <123109500+KarolinaLikhacheva@users.noreply.github.com>
2023-10-29 21:43:55 +0300

    Add files via upload

--- news/signals.py
+++ news/signals.py
@@ -1,9 +1,9 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, m2m_changed
+
 from django.dispatch import receiver
-from django.contrib.auth.models import User, Group
 from .models import PostCategory
 from .tasks.basic import new_post_subscription
-from django.db.models.signals import m2m_changed
+from django.contrib.auth.models import User, Group
 from django.core.mail import send_mail
 
 
@@ -14,7 +14,7 @@ from django.core.mail import send_mail
 #        common_group.user_set.add(instance)
 
 
-@receiver(m2m_changed, sender=PostCategory)
+@receiver(m2m_changed, sender=PostCategory)     # уведомляем пользователя о новом посте
 def notify_subscribers(sender, instance, **kwargs):
     if kwargs['action'] == 'post_add':
         pass
@@ -25,7 +25,7 @@ def notify_subscribers(sender, instance, **kwargs):
 #    if created:
 #        subject = 'Добро пожаловать в наше приложение'
 #        message = 'Здравствуйте, спасибо за регистрацию в нашем приложении. Надеемся, вам здесь понравится!'
-#        from_email = 'vasiliygolicin@yandex.ru'  # Замените на вашу электронную почту
+#        from_email = l-karolina-a@mail.ru'  # Замените на вашу электронную почту
 #        recipient_list = [instance.email]
 
 #        send_mail(subject, message, from_email, recipient_list, fail_silently=False)
